8189859dbe18af251d1e99528d4c162bf376ea01
gabriel <81696183+kebabzter@users.noreply.github.com>
2025-12-14 15:18:11 +0100

    Clean up auth middleware and enhance token validation
    Removed commented-out code and improved token validation handling.

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,12 +2,7 @@ const { validateToken } = require("../services/userService");
 
 const authMiddleware = (req, res, next) => {
     const token = req.headers["x-authorization"];
-    // const rawHeaders = req.rawHeaders
-    // if(!token) {
-    //     rawHeaders.forEach((h) => h.startsWith('auth-token') ? usertoken = h.split("=")[1] : null  )
-    //     console.log('user token:', usertoken)
-    // }
-    // console.log("User token 1:", token)
+    
     if(token){
         try {
             const user = validateToken(token);
@@ -20,7 +15,6 @@ const authMiddleware = (req, res, next) => {
         } catch (error) {
             res.status(401).json(error)
             console.log(error);
-            return;
         }
     }
     next();
